d1386904fc843c38c1f2b7f581b1fed23010361d
ahadtaghiyev <106157931+AhadDV@users.noreply.github.com>
2022-09-21 02:16:01 +0400

    Add files via upload
    created

---
+++ Script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+me=`basename "$0"`
+filename=${me##*.}
+mkdir $filename
+cd $filename
+dotnet new mvc -o $filename
+cd ./
+dotnet new sln --name $filename
+dotnet sln $filename.sln add ./$filename/$filename.csproj 
+cd $filename
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+cd Models
+echo "public class Product
+{
+  public int ProductId {get;set;}
+  
+}" >> Product.cs
+
+echo "public class Category
+{
+  public int CategoryId {get;set;}
+  public string CategoryName {get;set;}
+  public string CategoryDescription {get;set;}
+}" >> Category.cs
+
+
+
+
+
+
